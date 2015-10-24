81496b1e6ee14f6401d1d2805c2631dc7ccc3433
Pierre Rochard <pierre@rochard.org>
2015-10-24 18:10:22 -0400

    upped the monitoring

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
 from datetime import datetime
 from decimal import Decimal
+import pytz
 from trading import file_logger
 import argparse
+import numpy
 
 try:
     import ujson as json
@@ -57,7 +59,7 @@ def websocket_to_order_book():
     order_book.get_level3()
 
     [order_book.process_message(message) for message in messages if message['sequence'] > order_book.level3_sequence]
-
+    messages = []
     while True:
         message = yield from coinbase_websocket.recv()
         if message is None:
@@ -68,6 +70,15 @@ def websocket_to_order_book():
         except TypeError:
             file_logger.error('JSON did not load, see ' + str(message))
             return False
+        if args.command_line:
+            messages += [datetime.strptime(message['time'], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=pytz.UTC)]
+            messages = [message for message in messages if (datetime.now(tzlocal()) - message).seconds < 60]
+            if len(messages) > 2:
+                diff = numpy.diff(messages)
+                diff = [float(sec.microseconds) for sec in diff]
+                order_book.average_rate = numpy.mean(diff)
+                order_book.fastest_rate = min(diff)
+                order_book.slowest_rate = max(diff)
         if not order_book.process_message(message):
             print(pformat(message))
             return False
@@ -203,13 +214,9 @@ def market_maker_strategy():
 def buyer_strategy():
     time.sleep(10)
     while True:
-        time.sleep(0.01)
-        if order_book.asks.price_tree.min_key() - order_book.bids.price_tree.max_key() < 0:
-            file_logger.warn('Negative spread: {0}'.format(
-                order_book.asks.price_tree.min_key() - order_book.bids.price_tree.max_key()))
-            continue
+        time.sleep(0.001)
         if not open_orders.open_bid_order_id:
-            open_bid_price = order_book.asks.price_tree.min_key() - spreads.bid_spread - open_orders.open_bid_rejections
+            open_bid_price = order_book.bids.price_tree.max_key() - spreads.bid_spread
             if 0.01 * float(open_bid_price) < float(open_orders.accounts['USD']['available']):
                 order = {'size': '0.01',
                          'price': str(open_bid_price),
@@ -280,12 +287,14 @@ def monitor():
         time.sleep(0.001)
         print('Last message: {0:.6f} secs, '
               'Min ask: {1:.2f}, Max bid: {2:.2f}, Spread: {3:.2f}, '
-              'Your ask: {4:.2f}, Your bid: {5:.2f}, Your spread: {6:.2f}'.format(
+              'Your ask: {4:.2f}, Your bid: {5:.2f}, Your spread: {6:.2f} '
+              'Avg: {7:.10f} Min: {8:.10f} Max: {9:.10f}'.format(
             ((datetime.now(tzlocal()) - order_book.last_time).microseconds * 1e-6),
             order_book.asks.price_tree.min_key(), order_book.bids.price_tree.max_key(),
             order_book.asks.price_tree.min_key() - order_book.bids.price_tree.max_key(),
             open_orders.decimal_open_ask_price, open_orders.decimal_open_bid_price,
-            open_orders.decimal_open_ask_price - open_orders.decimal_open_bid_price), end='\r')
+            open_orders.decimal_open_ask_price - open_orders.decimal_open_bid_price,
+            order_book.average_rate*1e-6, order_book.fastest_rate*1e-6, order_book.slowest_rate*1e-6), end='\r')
 
 
 if __name__ == '__main__':
