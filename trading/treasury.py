db5d22a9d882038b63aa9787db99c1beb4797b1d
Pierre Rochard <pierre@rochard.org>
2015-10-27 08:05:55 -0400

    treasury second iteration

--- trading/treasury.py
+++ trading/treasury.py
@@ -1,13 +1,19 @@
 from pprint import pformat
 from datetime import date
 from coinbase.wallet.client import Client
+import requests
 from dateutil.parser import parse
 import time
-from trading.treasury_config import DEPOSIT_KEY, DEPOSIT_SECRET, PAYMENT_METHOD
+from trading import file_logger
+from trading.exchange import CoinbaseExchangeAuthentication, exchange_api_url
+from trading.treasury_config import (DEPOSIT_KEY, DEPOSIT_SECRET, PAYMENT_METHOD,
+                                     EXCHANGE_KEY, EXCHANGE_SECRET, EXCHANGE_PASSPHRASE)
 
 client = Client(DEPOSIT_KEY, DEPOSIT_SECRET)
 
 hours = 24
+
+exchange_auth = CoinbaseExchangeAuthentication(EXCHANGE_KEY, EXCHANGE_SECRET, EXCHANGE_PASSPHRASE)
 
 
 def view_payment_methods():
@@ -17,13 +23,18 @@ def view_payment_methods():
 
 def nostro_transfer():
     for account in client.get_accounts()['data']:
-        if account['currency'] == 'USD':
-            print(pformat(account))
+        if account['name'] == 'USD Wallet':
             deposits = account.get_deposits()['data']
-            print(pformat(deposits))
             if parse(deposits[0]['created_at']).date() < date.today():
                 account.deposit(payment_method=PAYMENT_METHOD, amount='50', currency='USD')
-    time.sleep(24*60*60)
+            if account['balance']['amount'] >= 50:
+                transfer = {'type': 'deposit',
+                            'amount': '50',
+                            'coinbase_account_id': account['id']}
+                response = requests.post(exchange_api_url + 'orders', json=transfer, auth=exchange_auth)
+                if response.status_code != 200:
+                    file_logger.error('Nostro transfer failed: {0}'.format(response.json()))
+    time.sleep(24 * 60 * 60)
 
 
 if __name__ == '__main__':
