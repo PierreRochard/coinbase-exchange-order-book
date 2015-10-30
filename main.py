c218401f288f6cf32b6a2b58ed1cce904c9e47ef
Pierre Rochard <pierre@rochard.org>
2015-10-30 19:22:34 -0400

    factoring out support utilities

--- main.py
+++ main.py
@@ -27,12 +27,10 @@ from trading.openorders import OpenOrders
 from trading.spreads import Spreads
 from orderbook.book import Book
 from trading.strategies import buyer_strategy
-from trading.treasury import nostro_transfer
 
 ARGS = argparse.ArgumentParser(description='Coinbase Exchange bot.')
 ARGS.add_argument('--c', action='store_true', dest='command_line', default=False, help='Command line output')
 ARGS.add_argument('--t', action='store_true', dest='trading', default=False, help='Trade')
-
 args = ARGS.parse_args()
 
 order_book = Book()
@@ -150,7 +148,6 @@ if __name__ == '__main__':
         loop.run_in_executor(None, functools.partial(buyer_strategy, order_book, open_orders, spreads))
         loop.run_in_executor(None, update_balances)
         loop.run_in_executor(None, update_orders)
-        loop.run_in_executor(None, nostro_transfer)
     if args.command_line:
         loop.run_in_executor(None, monitor)
     n = 0
